f94b88ecbbb0f6201e29520c9d80133e4829007f
Georgi Georgiev <ghgeorgiev@2create.studio>
2023-04-25 15:38:07 +0300

    Update some intelephense LSP settings so it can index bigger files n stuff

--- init.lua
+++ init.lua
@@ -36,21 +36,21 @@ return {
           -- "go",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
-          "blade",
+          -- "blade",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
       },
-      timeout_ms = 1000, -- default format timeout
+      timeout_ms = 10000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
     },
     -- enable servers that you already have installed without mason
     servers = {
-      -- "pyright"
+      -- "intelephense",
     },
   },
   -- Configure require("lazy").setup() options
--- lsp/config/intelephense.lua
+++ lsp/config/intelephense.lua
@@ -1,5 +1,23 @@
 return {
   on_attach = function(client, bufnr)
-    client.resolved_capabilities.document_formatting = false
+    if (client.resolved_capabilities ~= nil) then
+      client.resolved_capabilities.document_formatting = false
+    end
   end,
+  settings = {
+    intelephense = {
+      environment = {
+        includePaths = { "/path/to/stuff",
+          "/path/to/more_stuff"
+        },
+        phpVersion = "8.1.0",
+      },
+      files = {
+        maxSize = 2000000,
+      },
+      completion = {
+        maxItems = 100,
+      }
+    }
+  }
 }
