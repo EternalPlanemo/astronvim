2dcb5698f83225bc70a2d1631c472f760628be4d
Georgi Georgiev <ghgeorgiev@2create.studio>
2023-12-22 21:03:51 +0200

    Fix LuaSnip leaving ghost placeholders that cause surprise jumps

--- plugins/user.lua
+++ plugins/user.lua
@@ -12,8 +12,14 @@ return {
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
-      require "plugins.configs.luasnip" (plugin, opts)                                       -- include the default astronvim config that calls the setup call
-      require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } } -- load snippets paths
+      local luasnip_configs = require "plugins.configs.luasnip"
+        luasnip_configs(plugin, opts)  -- include the default astronvim config that calls the setup call
+
+        opts = opts or {}
+        opts.region_check_events = "CursorHold,InsertLeave"
+        opts.delete_check_events = "TextChanged,InsertInter"
+
+        require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } } -- load snippets paths
     end,
   },
   "sainnhe/edge",
