96f6c36b5dfa8c72aaff9656d6fd589dc110173a
Georgi Georgiev <ghgeorgiev@2create.studio>
2024-03-05 10:15:20 +0200

    Add tree-sitter-blade config

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,16 +1,31 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+    -- ---@class ParserInfo[]
+    -- local parser_config = require("nvim-treesitter.parsers").get_parser_configs();
+    --
+    -- parser_config.blade = {
+    --   install_info = {
+    --     url = "~/.config/nvim/lua/user/plugins/tree-sitter-blade",
+    --     files = { "src/parser.c" },
+    --     branch = "master",
+    --     generate_requires_npm = true,
+    --     requires_generate_from_grammar = true,
+    --   },
+    --   filetype = "blade"
+    -- }
+
   opts = {
     ensure_installed = {
       "c",
+      "cpp",
       "lua",
       "vim",
       "php",
       "rust",
       "javascript",
       "typescript",
-      "cpp",
       "twig",
+      "zig",
     },
-  },
+  }
 }
