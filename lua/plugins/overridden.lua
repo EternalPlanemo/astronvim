26e8aa4e14937ab0b4ddb537ba252c461002f5da
EternalPlanemo <eternalplanemo@proton.me>
2024-09-11 11:15:46 +0300

    Telescope: Add filesize_limit

---
+++ lua/plugins/overridden.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        preview = {
+          filesize_limit = 0.5555,
+        },
+      },
+    },
+  },
+}
