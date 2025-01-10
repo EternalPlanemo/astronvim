56bf9a72d4c491080777a85f2c8b25e22de2dd73
EternalPlanemo <eternalplanemo@proton.me>
2025-01-10 12:06:20 +0200

    Use php8.4

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -57,7 +57,7 @@ return {
         settings = {
           intelephense = {
             environment = {
-              phpVersion = "8.2.0",
+              phpVersion = "8.4.0",
             },
             files = {
               maxSize = 5000000,
