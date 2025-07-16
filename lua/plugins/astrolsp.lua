864bd5dc30b1f39dc36c49973ca1ba85d46e9d13
EternalPlanemo <eternalplanemo@proton.me>
2025-07-16 11:39:53 +0300

    Update core template

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -63,6 +63,7 @@ return {
         filetypes = { "php", "blade" },
         capabilities = {
           textDocument = {
+            formatting = nil,
             completion = {
               completionItem = {
                 snippetSupport = false,
@@ -260,25 +261,22 @@ return {
     -- Configure buffer local auto commands to add when attaching a language server
     autocmds = {
       -- first key is the `augroup` to add the auto commands to (:h augroup)
-      lsp_document_highlight = {
+      lsp_codelens_refresh = {
         -- Optional condition to create/delete auto command group
         -- can either be a string of a client capability or a function of `fun(client, bufnr): boolean`
         -- condition will be resolved for each client on each execution and if it ever fails for all clients,
         -- the auto commands will be deleted for that buffer
-        cond = "textDocument/documentHighlight",
+        cond = "textDocument/codeLens",
         -- cond = function(client, bufnr) return client.name == "lua_ls" end,
         -- list of auto commands to set
         {
           -- events to trigger
-          event = { "CursorHold", "CursorHoldI" },
+          event = { "InsertLeave", "BufEnter" },
           -- the rest of the autocmd options (:h nvim_create_autocmd)
-          desc = "Document Highlighting",
-          callback = function() vim.lsp.buf.document_highlight() end,
-        },
-        {
-          event = { "CursorMoved", "CursorMovedI", "BufLeave", "InsertLeave" },
-          desc = "Document Highlighting Clear",
-          callback = function() vim.lsp.buf.clear_references() end,
+          desc = "Refresh codelens (buffer)",
+          callback = function(args)
+            if require("astrolsp").config.features.codelens then vim.lsp.codelens.refresh { bufnr = args.buf } end
+          end,
         },
       },
     },
@@ -306,7 +304,6 @@ return {
 
       -- this would disable semanticTokensProvider for all clients
       -- client.server_capabilities.semanticTokensProvider = nil
-      if client.name == "intelephense" then client.capabilities.textDocument.formatting = nil end
 
       if client.name == "html" and filetype == "php" then client.server_capabilities.renameProvider = nil end
     end,
