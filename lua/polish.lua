2abebdf98fb3377c203b086f71656d299fecb395
EternalPlanemo <eternalplanemo@proton.me>
2026-04-02 11:01:17 +0300

    Fix font

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,11 +1,8 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
-if vim.g.neovide and jit.os == "OSX" then vim.g.neovide_scale_factor = 1.25 end
-if vim.g.neovide and jit.os == "Linux" then vim.g.neovide_opacity = 0.9 end
+vim.o.guifont = "FiraCode Nerd Font Mono:h16"
 
 if vim.g.neovide and jit.os == "OSX" then
   vim.keymap.set(
